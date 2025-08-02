977eae4bdc62dc98c226db6a0c3009a88831ed02
Mohammad Farouk <phun.for.physics@gmail.com>
2025-08-02 19:28:46 +0300

    Pump up the version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'profilefield_phone';
-$plugin->release = '1.2.0';
-$plugin->version = 2025080200;
+$plugin->release = '1.2.2';
+$plugin->version = 2025080207;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
